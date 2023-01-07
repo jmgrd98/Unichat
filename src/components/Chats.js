3877b63da02dd5382d7436f94ac9931dcbeddf4a
João Marcelo Dantas <jmgrd98@gmail.com>
2023-01-07 10:50:22 -0300

    commit

--- src/components/Chats.js
+++ src/components/Chats.js
@@ -1,8 +1,40 @@
 import React from "react";
+import { useHistory } from "react-router-dom";
+import {ChatEngine} from 'react-chat-engine'
+import { auth } from "../firebase";
+import '../index.css'
+import {useAuth} from '../contexts/AuthContext'
 
 export default function Chats(){
 
+const history = useHistory()
+const {user} = useAuth()
+
+
+    async function handleLogout(){
+        await auth.signOut()
+        history.push('/')
+    }
+
     return(
-        <h1>Chats</h1>
+        <div className="chats-page">
+            <div className="nav-bar">
+                <div className="logo-tab">
+                    Unichat
+                </div>
+                <div
+                className="logout-tab"
+                onClick={handleLogout}>
+                    Logout
+                </div>
+            </div>
+
+            <ChatEngine
+                height='calc(100vh - 66px)'
+                projectId='a0ac4f3d-7768-4e96-81e6-4ed6436ae06d'
+                userName='.'
+                userSecret='.'
+            />
+        </div>
     )
 }
